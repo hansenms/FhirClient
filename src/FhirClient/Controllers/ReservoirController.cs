b082e0748a4ba2f6a74a1d65274e1b970754df0c
Michael Hansen <mihansen@microsoft.com>
2018-10-16 08:23:23 -0700

    Some debug changes

--- src/FhirClient/Controllers/ReservoirController.cs
+++ src/FhirClient/Controllers/ReservoirController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using FhirClient.Models;
 using FhirClient.Services;
+using System.Net.Http;
 
 namespace FhirClient.Controllers
 {
@@ -21,8 +22,21 @@ namespace FhirClient.Controllers
         public async Task<IActionResult> Index()
         {
             ViewData["NumberOfPatients"] = await PatientReservoir.GetNumberOfPatientsAsync();
-            ViewData["NextPatient"] = await PatientReservoir.GetNextPatientJson();
+            //ViewData["NextPatient"] = await PatientReservoir.GetNextPatientJson();
+            ViewData["NextPatient"] = Request.Headers["X-ZUMO-AUTH"];
             return View();
+        }
+
+        public async Task<string> Refresh()
+        {
+            using (var client = new HttpClient())
+            {
+                string prefix = Request.IsHttps? "https" : "http";
+                Console.WriteLine($"{prefix}://{Request.Host.ToString()}");
+                client.BaseAddress = new Uri($"{prefix}://{Request.Host.ToString()}");
+                var result = await client.GetAsync("/.auth/refresh");
+                return await result.Content.ReadAsStringAsync();
+            }
         }
     }
 }
