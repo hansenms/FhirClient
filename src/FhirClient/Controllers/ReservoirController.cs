aeaeba3db4fce3acbf42e663889146c508aa873f
Michael Hansen <mihansen@microsoft.com>
2018-10-16 10:22:08 -0700

    added refresh_token.html

--- src/FhirClient/Controllers/ReservoirController.cs
+++ src/FhirClient/Controllers/ReservoirController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using FhirClient.Models;
 using FhirClient.Services;
 using System.Net.Http;
+using System.Net;
 
 namespace FhirClient.Controllers
 {
@@ -29,12 +30,15 @@ namespace FhirClient.Controllers
 
         public async Task<string> Refresh()
         {
-            using (var client = new HttpClient())
+            string prefix = Request.IsHttps? "https" : "http";
+            var baseAddress = new Uri($"{prefix}://{Request.Host.ToString()}");
+            var cookieContainer = new CookieContainer();
+            using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
+            using (var client = new HttpClient(handler) { BaseAddress = baseAddress })
             {
-                string prefix = Request.IsHttps? "https" : "http";
-                Console.WriteLine($"{prefix}://{Request.Host.ToString()}");
-                client.BaseAddress = new Uri($"{prefix}://{Request.Host.ToString()}");
+                cookieContainer.Add(baseAddress, new Cookie("AppServiceAuthSession", Request.Cookies["AppServiceAuthSession"]));
                 var result = await client.GetAsync("/.auth/refresh");
+                result.EnsureSuccessStatusCode();
                 return await result.Content.ReadAsStringAsync();
             }
         }
